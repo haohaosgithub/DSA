e27d5e471fd1f0f5ffa043409aaf1cad31d5dc5c
haohao <hh758009610@163.com>
2023-04-18 19:00:39 +0800

    [位运算] 异或 1.一个数组中，出现奇数次的只有一种数，求这个数 2.一个数组中，出现奇数次的只有两种数，求这个两个数

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using DSA;
+using System.Runtime.InteropServices;
 
 internal class Program
 {
@@ -6,16 +7,22 @@ internal class Program
     {
         #region 向量测试
         //Vector<int> v = new Vector<int>();
-        int[] array = { 4,3,2,4 };
-        Vector<int> v = new Vector<int>(array);
+        //int[] array = { 4,3,2,4 };
+        //Vector<int> v = new Vector<int>(array);
         //v.SelectionSort();
         //v.BubbleSort();
         //v.InsertionSort();
 
         //Console.WriteLine(v.BinarySearch(8));
         //Console.WriteLine(v.BinarySearchLargeEq(3));
-        Console.WriteLine(v.LocalMin());
+        //Console.WriteLine(v.LocalMin());
         #endregion
-
+        #region 异或问题测试
+        //int[] xorarr = { 1,1,1,1,2,3,3};
+        //Console.WriteLine(XOR.FindOnlyOnce(xorarr));
+        int[] xorarr2 = { 1, 1, 1, 1, 2, 2, 2, 3, 4, 4 };
+        XOR.FindOnlyTwice(xorarr2,out int a,out int b);
+        Console.WriteLine(a + " " + b);
+        #endregion
     }
 }
---
+++ XOR.cs
@@ -0,0 +1,46 @@
+﻿namespace DSA
+{
+    /// <summary>
+    /// 异或问题
+    /// 基础性质： 
+    /// 1.相同为0，不同为1 
+    /// 2.可以理解为无进位的二进制加法  
+    /// 3.满足结合律和交换律
+    /// 4.N^N = 0 0 ^N = N
+    /// </summary>
+    public static class XOR
+    {
+        //一个数组中 有一种数出现了奇数次，另一种数都出现了偶数次，找到这个数
+        public static int FindOnlyOnce(int[] arr)
+        {
+            int xor = 0;
+            foreach (var item in arr) //偶数项全部放一起异或，结果为0，再异或一个数，结果即为这个数
+            {
+                xor ^= item;
+            }
+            return xor;
+        }
+
+        //一个数组中 有两种数出现了奇数次，另一种数都出现了偶数次，找到这个数
+        public static void FindOnlyTwice(int[] arr, out int a, out int b)
+        {
+            int xor = 0;
+            int xor1 = 0;
+            foreach (var item in arr) //偶数项全部放一起异或，结果为0，再异或一个数，结果即为两个出现了奇数次异或的数的异或
+            {
+                xor ^= item;
+            }
+            //此时可以理解为只剩 1个 i 和一个 j 异或，这个值为xor
+            //int rightOne = xor & (~xor + 1); //x 取反+1 相当于将最右边的1左边的所有数取反，此时  x 与取反加一（-x）的唯一共同点是最右边的1，rightOne表示最右边的1，其他位全为0代表的数
+            int rightOne = xor & (-xor);
+            foreach (var item in arr)
+                if ((item & rightOne) == 1) //某一位为1的数的异或，最终由于交换律和结合律，只剩两个数各出现一次，因为该位为1，所以两个数一个在该位为1，一个在该位为0
+                {
+                    xor1 ^= item;
+                }
+            a = xor1;
+            b = a ^ xor;
+        }
+    }
+}
+
